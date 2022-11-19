51f1d5a37a5537d005a0ed36b44bebf45a59774c
Hesham Ragab Aly Hussein <heshamrobert21@gmail.com>
2022-11-19 19:04:33 +0200

    Time: 0 ms (100.00%), Space: 6.8 MB (96.79%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
+	int fr[255];
     int lengthOfLongestSubstring(string s) {
-    	int p1 = 0, p2 = 0, ans = 0, cnt = 0;
-    	set<char> fr;
-    	while(p2 < s.size())
+    	int p1 = 0, p2 = 0, ans = 0;
+    	memset(fr, -1, sizeof(fr));
+    	while(p2 < (int)s.size())
     	{
-    		while(p2 < s.size() and !fr.count(s[p2]))
+    		int ch = s[p2];
+    		int id = fr[ch];
+
+    		if((~id) and p1 <= id and id < p2)
     		{
-    			fr.insert(s[p2]);
-    			p2++;
-    			cnt++;
+    			p1 = id + 1;
     		}
-    		//system("sleep 1");
-    		//cerr << cnt << " " << p1 << " " << p2 << " " << s[p2] << endl;
-    		ans = max(ans, cnt);
-    		while(p1 < s.size() and fr.count(s[p2]))
-    		{
-    			fr.erase(s[p1]);
-    			p1++;
-    			cnt--;
-    		}
+    		//cerr << p1 << " " << p2 << endl;
+    		ans = max(ans, p2 - p1 + 1);
+    		fr[ch] = p2;
+    		p2++;
     	}
     	return ans;
     }

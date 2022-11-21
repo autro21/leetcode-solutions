c2149687220a82ad6824103c4039990ac0169c14
Hesham Ragab Aly Hussein <heshamrobert21@gmail.com>
2022-11-21 09:53:43 +0200

    Time: 41 ms (63.44%), Space: 7.8 MB (64.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-	int dp[1005][1005];
-	bool ispal(int i, int j, const string& s)
+	void converter(string& s)
 	{
-		if(i >= j)
-			return true;
-		int& ret = dp[i][j];
-		if(~ret)
-			return ret;
-		ret = s[i] == s[j];
-		ret &= ispal(i + 1, j - 1, s);
-		return ret;
+		string tmp = "|";
+		for(auto ch: s)
+		{
+			tmp += ch;
+			tmp += '|';
+		}
+		s = tmp;
 	}
 	string longestPalindrome(string s) {
 		string ret = "";
-		int ii, jj, mx = 0;
-		memset(dp, -1, sizeof dp);
-		for(int i = 0; i < s.size();++i)
+		converter(s);
+
+		int i, j, mx = 0;
+		for(int c = 0; c < (int)s.size(); ++c)
 		{
-			for(int j = s.size() - 1; j >= i; --j)
+			int r = 0;
+			while(c - (r + 1) >= 0 and c + (r + 1) and s[c - (r + 1)] == s[c + (r + 1)])
 			{
-				if(ispal(i, j, s))
-				{
-					//cerr << i << " " << j << " " << j - i + 1 <<  endl;
-					if(j - i + 1 > mx)
-					{
-						ii = i;
-						jj = j;
-						mx = j - i + 1;
-					}
-				}
+				r++;
+			}
+			int sz = r*2 + 1;
+			if(sz > mx)
+			{
+				i = c - r;
+				j = c + r;
+				mx = sz;
 			}
 		}
-		for(int i = ii ; i <= jj; ++i)
-			ret += s[i];
+		for(int ii  = i ; ii <= j; ++ii)
+			if(s[ii] != '|')ret += s[ii];
 		return ret;
     }
 };

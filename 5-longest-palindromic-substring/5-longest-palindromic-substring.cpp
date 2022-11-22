0265f05890b7c326986b2cb03fee8bc46c7dcb6d
Hesham Ragab Aly Hussein <heshamrobert21@gmail.com>
2022-11-22 22:27:17 +0200

    Time: 14 ms (91.98%), Space: 8.7 MB (59.44%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -13,25 +13,35 @@ public:
 	string longestPalindrome(string s) {
 		string ret = "";
 		converter(s);
-
-		int i, j, mx = 0;
-		for(int c = 0; c < (int)s.size(); ++c)
+		//manchars' algorithm
+		vector<int> p(s.size() + 5, 0);
+		int m = p[0] = 0;
+		for(int i = 1; i < (int)s.size(); ++i)
 		{
-			int r = 0;
-			while(c - (r + 1) >= 0 and c + (r + 1) and s[c - (r + 1)] == s[c + (r + 1)])
+			int mirror = 2 * m - i;
+			mirror = mirror < 0 ? 0:mirror;
+			int&curr = p[i] = p[mirror];
+			curr = min(curr, m + p[m] - i);
+			while(i + curr + 1 < (int)s.size() and i - curr - 1 >= 0 and s[i+curr+1] == s[i - curr - 1])curr++;
+
+			if(p[i]+i > p[m]+m)
+				m = i;
+		}
+		int mx = 0, st, en;
+		for(int i = 1; i < (int)s.size();++i)
+		{
+			//cerr << i << " " <<  p[i] << endl;
+			if(p[i] > mx)
 			{
-				r++;
-			}
-			int sz = r*2 + 1;
-			if(sz > mx)
-			{
-				i = c - r;
-				j = c + r;
-				mx = sz;
+				mx = p[i];
+				st = i - p[i];
+				en = i + p[i];
 			}
 		}
-		for(int ii  = i ; ii <= j; ++ii)
-			if(s[ii] != '|')ret += s[ii];
+		//cerr << st << endl;
+		for(int i = st; i < en; ++i)
+			if(s[i] != '|')
+				ret += s[i];
 		return ret;
     }
 };
